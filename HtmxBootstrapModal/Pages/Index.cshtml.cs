8cbacd1e59270785a2184bef3a4635e0166b0090
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-06-07 09:10:27 -0400

    Initial Commit

---
+++ HtmxBootstrapModal/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HtmxBootstrapModal.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    
+    [BindProperty]
+    public string Message { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnGetModal()
+    {
+        return Partial("Modal");
+    }
+
+    public IActionResult OnPostModal()
+    {
+        return Partial("Success", this);
+    }
+}
